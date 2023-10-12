9a73a3d593d0d8eeb35bafd819d3630a15cffdc8
akorbutovski <akorbutovski@elinext.com>
2023-10-12 19:24:58 +0300

    feat: common stream for array (Set, Map)

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// const {rxjs} = require('rxjs')
-// import * as rxjs from 'rxjs';
-// import { of, interval, take, timer } from 'rxjs';
-
 const setSubscriber = (operatorName) => {
     return {
         next(x) {
@@ -12,29 +8,16 @@ const setSubscriber = (operatorName) => {
             console.log(`${operatorName}: Completed.`)}
     }
 }
+const arrObj = [
+    {id: 1, name: 'alex'},
+    {id: 1, name: 'tania'},
+    {id: 1, name: 'pavel'},
+    {id: 1, name: 'illya'}
+]
+// const set = new Set([1,2,3,'4', {id:0}])
+const set = new Map([[1,2], [3,4], ['5','6']])
 
-// rxjs.of(4,2,3,7,0,'6',22,'str', [11,22,33])
-//     .subscribe(setSubscriber('of'))
-
-// rxjs.interval(100)
-//     .pipe(rxjs.take(10))
-//     .subscribe(setSubscriber('interval'))
-
-// rxjs.timer(2000, 100)
-//     .pipe(rxjs.take(10))
-//     .subscribe(setSubscriber('timer'))
-
-rxjs.range(5, 15)
-    .pipe(rxjs.take(10))
-    .subscribe(setSubscriber('range'))
-
-// const btn = document.querySelector('button');
-// const input = document.querySelector('input');
-// const button$ = rxjs.fromEvent(btn,'click')
-// const input$ = rxjs.fromEvent(input,'keydown')
-// const document$ = rxjs.fromEvent(document,'mousemove')
-
-// button$.subscribe(e => {console.log(e)})
-// input$.subscribe(e => {console.log(e)})
-// document$.subscribe(e => {
-//     document.querySelector('h2').innerHTML = `X: ${e.clientX}, Y: ${e.clientY}`})
+// rxjs.from([1,3,5,7,9])
+// rxjs.from(arrObj)
+rxjs.from(set)
+    .subscribe(setSubscriber('from'))
