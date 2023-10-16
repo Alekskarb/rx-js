e9144104a9aeb0ad7bc8f75bfc6855dd10d8dbfe
Alex Korbutovski <akorbutovski@elinext.com>
2023-10-16 20:07:27 +0300

    feat: filter operators (find take skip)

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-// const {take} = require("rxjs");
-// import {concat, interval, take} from "rxjs";
-
+// const { first } = rxjs;
+// const { filter, map } = rxjs.operators;
 const setSubscriber = (operatorName) => {
     return {
         next(x) {
@@ -15,34 +14,27 @@ const setSubscriber = (operatorName) => {
     }
 }
 
-const stream1$ = rxjs.of('Hello')
-const stream2$ = rxjs.of('World')
+// operator for CHOOSE
 
-const stream3$ = rxjs.interval(1000).pipe(rxjs.map(e => "Stream3: " + e))
-const stream4$ = rxjs.interval(500).pipe(rxjs.map(e => "Stream4: " + e))
+rxjs.of(1,42,3, '4', "Angular", true)
+    .pipe(
+        // rxjs.first(),
+        // rxjs.last())
+        // rxjs.find(el => el===3))
+        // rxjs.findIndex(el => el=== 1))
+        // rxjs.take(3))
+        // rxjs.skip(3))
+        rxjs.skipWhile(el => {
+            return typeof el === 'number'
+        }))
+    .subscribe(
+        setSubscriber('map')
+    )
 
-stream1$.pipe(
-    rxjs.mergeWith(stream2$))
-    .subscribe(setSubscriber('merge'))
-
-// rxjs.mergeWith(stream3$, stream4$).pipe(rxjs.take(12))
-//     .subscribe(setSubscriber('merges'))
-
-rxjs.range(1,3).pipe(
-    rxjs.map(x => rxjs.range(1,3)),
-    rxjs.mergeAll())
-    .subscribe(setSubscriber('mergeAll'))
-
-const timer1 = rxjs.interval(1000).pipe(rxjs.take(10));
-const timer2 = rxjs.interval(2000).pipe(rxjs.take(6));
-const timer3 = rxjs.interval(500).pipe(rxjs.take(10));
-
-const result = rxjs.concat(timer1, timer2, timer3);
-result.subscribe(x => console.log(x));
-
-const stream5$ = rxjs.from(['Hello'])
-const stream6$ = rxjs.from(['World'])
-stream6$.pipe(
-//     rxjs.concat(stream5$, stream6$)
-rxjs.concatWith(stream5$))
-    .subscribe(setSubscriber('concat'))
+rxjs.interval(500)
+    .pipe(
+        rxjs.skipUntil(rxjs.timer(3000)),
+        rxjs.takeUntil(rxjs.timer(5000)))
+    .subscribe(
+        setSubscriber('map')
+    )
