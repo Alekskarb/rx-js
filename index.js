1fb1b93473ef58e534147b2059e93945a7b7ea48
akorbutovski <akorbutovski@elinext.com>
2023-10-16 14:54:12 +0300

    feat: realize buffers

--- index.js
+++ index.js
@@ -11,29 +11,17 @@ const setSubscriber = (operatorName) => {
         }
     }
 }
-const cars = [
-    {name: 'audi', price: 40000},
-    {name: 'volvo', price: 30000},
-    {name: 'seat', price: 20000},
-]
 
-rxjs.fromEvent(document.querySelector('input'), 'keyup')
-    .pipe(rxjs.map(e => e.target.value))
-    .subscribe( x => rxjs.from(cars)
-            .pipe(rxjs.filter(car => car.name === x))
-            .subscribe(v => {
-                document.querySelector('div').innerHTML = `<h2>${v.name.toUpperCase()}</h2><h4>${v.price}</h4>`
-            })
-        // setSubscriber('filter')
-    )
+// rxjs.interval(500)
+// .pipe(rxjs.buffer(rxjs.interval(2000)),
+// // .pipe(rxjs.bufferTime(2000),
+//     rxjs.take(4)
+// ).subscribe(setSubscriber('buffer'))
+//
+// rxjs.range(0, 40)
+//     .pipe(rxjs.bufferCount(10)
+// ).subscribe(setSubscriber('bufferCount'))
 
- rxjs.fromEvent(document.querySelector('input'), 'keyup')
-    .pipe(rxjs.map(e => e.target.value),
-        // rxjs.debounceTime(1000)
-        rxjs.distinct()
-    )
-     .subscribe(setSubscriber('debounceTime'))
-
-rxjs.from([1,2,3,4,4,4,45,8,9,87, 'keyup'])
-    .pipe(rxjs.distinctUntilChanged())
-     .subscribe(setSubscriber('debounceTime'))
+rxjs.interval(500)
+    .pipe(rxjs.buffer(rxjs.fromEvent(document, 'click'))
+).subscribe(setSubscriber('bufferClick'))
