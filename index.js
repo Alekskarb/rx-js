a444bd3f5d5ce7d786570d6a12e0ac1e2abbf14c
akorbutovski <akorbutovski@elinext.com>
2023-10-16 18:33:14 +0300

    feat: realize merge & concat (join)

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// const {take} = require("rxjs");
 const setSubscriber = (operatorName) => {
     return {
         next(x) {
@@ -12,22 +13,27 @@ const setSubscriber = (operatorName) => {
     }
 }
 
-rxjs.of()
-    .pipe(rxjs.defaultIfEmpty('Some mess')
-    ).subscribe(setSubscriber('defaultIfEmpty'))
+const stream1$ = rxjs.of('Hello')
+const stream2$ = rxjs.of('World')
 
-// rxjs.from([1,2,3,4,5])
-rxjs.range(1, 3)
-    // .pipe(rxjs.every(el => el % 2 === 0)
-    // .pipe(rxjs.tap(el => console.log(el)),
-    .pipe(rxjs.map(x => x * x),
-        // rxjs.delay(1000)
-    ).subscribe(setSubscriber('every'))
+const stream3$ = rxjs.interval(1000).pipe(rxjs.map(e => "Stream3: " + e))
+const stream4$ = rxjs.interval(500).pipe(rxjs.map(e => "Stream4: " + e))
 
+stream1$.pipe(
+    rxjs.mergeWith(stream2$))
+    .subscribe(setSubscriber('merge'))
 
-rxjs.range(1, 3)
-    // .pipe(rxjs.every(el => el % 2 === 0)
-    // .pipe(rxjs.tap(el => console.log(el)),
-    // .map(x => x + x)
-    .pipe(observer => observer.map(x => x * x)
-    ).subscribe(setSubscriber('every'))
+// rxjs.mergeWith(stream3$, stream4$).pipe(rxjs.take(12))
+//     .subscribe(setSubscriber('merges'))
+
+rxjs.range(1,3).pipe(
+    rxjs.map(x => rxjs.range(1,3)),
+    rxjs.mergeAll())
+    .subscribe(setSubscriber('mergeAll'))
+
+const stream5$ = rxjs.from(['Hello'])
+const stream6$ = rxjs.from(['World'])
+stream5$.pipe(
+//     rxjs.concat(stream5$, stream6$)
+rxjs.concatWith(stream6$))
+    .subscribe(setSubscriber('concat'))
